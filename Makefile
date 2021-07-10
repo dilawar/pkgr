5d2da332d25e4d21c0c506a59203a3790b6e5690
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-07-10 20:53:47 +0000

    Updated.

--- Makefile
+++
@@ -1,7 +0,0 @@
-PKGR := poetry run pkgr
-
-all : create_pkg
-
-create_pkg: pkgr.toml
-	$(PKGR) rpm generate centos
-	# $(PKGR) rpm build centos
--- pkgr/deb.py
+++
@@ -1,9 +0,0 @@
-__author__ = "Dilawar Singh"
-__email__ = "dilawar.s.rajput@gmail.com"
-
-import typer
-
-app = typer.Typer()
-
-if __name__ == "__main__":
-    app()
