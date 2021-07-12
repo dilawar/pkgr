8079802fd9967c99e8815d27bc0c6cf131cd12d1
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-07-12 09:49:20 +0000

    rpm is populated with toml data.

--- pkgr/rpm.py
+++ pkgr/rpm.py
@@ -17,6 +17,39 @@ import typer
 
 app = typer.Typer()
 
+SPEC_TEMPLATE: str = """
+Name:       {name}
+Version:    {version}
+Release:    1%{{?dist}}
+Summary:    {summary}
+
+License:    {license}
+URL:        {url}
+Source0:    {source}
+
+{build_requires}
+
+{requires}
+
+%description
+{description}
+
+%prep
+{configure}
+
+%build
+{build}
+
+%install
+{install}
+
+%files
+{files}
+
+%changelog
+{changelog}
+"""
+
 
 def _gen_build_requires(builddeps: T.List[str]) -> str:
     return "\n".join([f"BuildRequires: {x}" for x in builddeps])
@@ -27,18 +60,19 @@ def _gen_requires(reqs: T.List[str]) -> str:
 
 
 def generate_spec_str() -> str:
-    RPM_TEMPLATE = pkgr.templates.RPM
-    config = pkgr.config.config()
-    config["build_requires"] = _gen_build_requires(config["builddeps"].get("rpm", []))
-    config["requires"] = _gen_requires(config.get("deps", dict(rpm=[])).get("rpm", []))
-    config["prep"] = pkgr.config.get(config, "prep", "%autosetup")
-    config["build"] = pkgr.config.get(config, "build", "%configure\n%make_build")
-    config["install"] = pkgr.config.get(
-        config, "install.bash", "rm -rf $RPM_TEMPLATE\n%make_install"
+    options = {}
+    options["build_requires"] = _gen_build_requires(options["builddeps"].get("rpm", []))
+    options["requires"] = _gen_requires(
+        options.get("deps", dict(rpm=[])).get("rpm", [])
     )
-    config["files"] = pkgr.config.get(config, "files", [])
-    config["changelog"] = pkgr.config.get(config, "changelog", [])
-    return RPM_TEMPLATE.format(**config)
+    options["optionsure"] = pkgr.config.get_val("optionsure.spec") or "%autosetup"
+    options["build"] = pkgr.config.get_val("build.rpm") or "%optionsure\n%make_build"
+    options["install"] = (
+        pkgr.config.get_val("install.rpm") or "rm -rf $SPEC_TEMPLATE\n%make_install"
+    )
+    options["files"] = pkgr.config.get_val("files") or ""
+    options["changelog"] = pkgr.config.get_val("changelog.rpm") or ""
+    return SPEC_TEMPLATE.format(**options)
 
 
 def default_dockerfile(distribution, release) -> Path:
